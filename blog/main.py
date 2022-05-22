47873c778cf84411e99006fc46ddd96524751c80
Artur Rejment <arturrejment1@gmail.com>
2022-05-22 15:09:31 +0200

    feat: create and save blog instance to the database

--- blog/main.py
+++ blog/main.py
@@ -1,15 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 
 from blog.schemas import BlogSchema
 from blog.models import BlogModel
-from blog.database import engine
+from blog.database import engine, SessionLocal
 
 
 app = FastAPI()
 
 BlogModel.metadata.create_all(bind=engine)
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @app.post('/blog')
-def create_blog(blog: BlogSchema):
-    return {'new_blog': blog}
+def create_blog(request: BlogSchema, db: Session = Depends(get_db)):
+    new_blog = BlogModel(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
