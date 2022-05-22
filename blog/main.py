ffade6419beb45854def7c60472c3fdc7b5fc48d
Artur Rejment <arturrejment1@gmail.com>
2022-05-22 17:47:32 +0200

    feat: fetch objects from db, return proper status codes

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response
 from sqlalchemy.orm import Session
 
 from blog.schemas import BlogSchema
@@ -18,10 +18,25 @@ def get_db():
         db.close()
 
 
-@app.post('/blog')
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
 def create_blog(request: BlogSchema, db: Session = Depends(get_db)):
     new_blog = BlogModel(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+
+@app.get('/blog')
+def all_blogs(db: Session = Depends(get_db)):
+    blogs = db.query(BlogModel).all()
+    return blogs
+
+
+@app.get('/blog/{blog_id}', status_code=status.HTTP_200_OK)
+def specific_blog(blog_id: int, response: Response, db: Session = Depends(get_db)):
+    blog = db.query(BlogModel).filter(BlogModel.id == blog_id).first()
+    if not blog:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return {'error': f'Blog with id "{blog_id}" does not exist in the database'}
+    return blog
