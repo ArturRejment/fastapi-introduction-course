ba29a3fe3a835ce12e3030338f6776ac2db584bc
Artur Rejment <arturrejment1@gmail.com>
2022-05-22 13:32:17 +0200

    feat: path and query params

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+from typing import  Optional
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get('/')
-async def root():
-    return {"message": "Hello World"}
+@app.get('/blog')
+def index(limit: int = 10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+
+    return {'data': f'{limit} unpublished blogs from the db'}
+
+
+@app.get('/blog/{blog_id}')
+def show(blog_id: int):
+    return {'blog': blog_id}
